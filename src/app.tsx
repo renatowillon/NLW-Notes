ad36b3c64ffbcb4dc0c8de575e88b7520d8bdefb
“renatowillon” <renatowillon@hotmail.com>
2024-02-09 17:50:04 -0300

    Final de Semana

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,46 @@
+import { ChangeEvent, useState } from 'react'
 import logo from '../src/assets/logo-nlw-escuro.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-
+interface Note {
+  id: string
+  data: Date
+  content: string
+}
 
 export function App() {
+
+const [search, setSearch] = useState('')
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+    if (notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+    return[]
+  })
+
+  function onNoteCreated(content:String){
+    const newNote = {
+      id: crypto.randomUUID(),
+      data: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+    setNotes(notesArray)
+    
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  console.log(search)
+
   return(
     <div className="max-w-6xl mx-auto my-12 space-y-6">
       <img src={logo} alt="NLW" />
@@ -14,18 +50,18 @@ export function App() {
           type="text" 
           placeholder='Busque em suas notas...'
           className="w-full bg-transparent text-3xl font-semibold outline-none tracking-tight placeholder: text-slate-500"
+          onChange={handleSearch}
         />
 
       </form>
       
       <div className="h-px bg-slate-700" />
         <div className="grid grid-cols-3  gap-6 auto-rows-[250px]">
-          <NewNoteCard />
+          <NewNoteCard onNoteCreated={onNoteCreated} />
 
-          <NoteCard note={{
-              data: new Date(), 
-              content: 'Hello World'
-          }} />
+          {notes.map(note => {
+            return <NoteCard key={note.id} note={note} />
+          })}
         </div>
       </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -16,7 +16,7 @@ export function NoteCard({ note }: NoteCardProps){
     <Dialog.Root>
         <Dialog.Trigger className=" rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 relative overflow-hidden outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-            {note.data.toISOString()}
+            {formatDistanceToNow(note.data, {locale: ptBR, addSuffix: true} )}
         </span>
         <p className="text-sm leading-6 text-slate-400">
             {note.content}
